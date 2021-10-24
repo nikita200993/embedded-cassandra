83bdabc08fa3f2c0823832a47b788e505bccc036
NikitaRR <nikita200993@gmail.com>
2021-10-25 02:07:45 +0300

    write version to file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,18 @@
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import org.apache.cassandra.cql3.QueryProcessor;
 import org.apache.cassandra.db.ConsistencyLevel;
 import org.apache.cassandra.service.CassandraDaemon;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("all okk");
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) throws IOException {
         System.setProperty(
                 "cassandra.config",
                 Main.class.getClassLoader().getResource("cassandra.yaml").toString()
@@ -17,18 +22,17 @@ public class Main {
                 "cassandra.storagedir",
                 userHome.resolve("cassandra").resolve("data").toString()
         );
-        final var originalStdOut = System.out;
         final var cassandraDaemon = CassandraDaemon.getInstanceForTesting();
         cassandraDaemon.activate();
         while (!cassandraDaemon.setupCompleted()) {
             Thread.onSpinWait();
         }
-        originalStdOut.println("**************************************************************************************************************************");
-        final var rows = QueryProcessor.execute(
+        final var version = QueryProcessor.execute(
                         "select release_version from system.local",
                         ConsistencyLevel.ALL)
                 .one()
                 .getString("release_version");
-        System.out.println(rows);
+        Files.writeString(Path.of("version.txt"), version);
+        cassandraDaemon.deactivate();
     }
 }
